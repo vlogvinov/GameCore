aace84c43094f2da96f9bd1328164d075a040e8d
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2017-10-02 23:42:00 +0300

    project init

---
+++ GameCore/PlazerCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore
+{
+    public class PlazerCharacter
+    {
+        public int Health { get; private set; } = 100;
+        public bool IsDead { get; private set; }
+
+        public void Hit(int damage)
+        {
+            Health -= damage;
+            if(Health <= 0)
+            {
+                IsDead = true;
+            }
+        }
+    }
+}
